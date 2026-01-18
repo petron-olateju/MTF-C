cd4056b4f4703a0e27f9f66a80d9d36ba8dadc92
Emmanuel Olateju <oluwasegun@aims.ac.za>
2026-01-18 22:34:05 +0000

    cross_validation pipeline implemented

--- cross_validation.py
+++ cross_validation.py
@@ -8,105 +8,181 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 
 from utils.metrics import accuracy_score
-from utils.data_loader import EEGDataset
+from utils.data_loader import EEGDataset, load_BNCI2014_001
 from models.DBConformer import DBConformer
 
-device = 'cpu'  # --> Update to Parameter object
-verbose = True  # --> set to command line argument
+import argparse
+from argparse import Namespace, ArgumentParser
 
-# ====================
-# DATA LOADING & SPLITTING
-# ====================
-X = np.random.randn((100, 1, 9, 1000))      # --> Replacce with loader class from utils.dataset_loader 
-y = np.random.randint(0, 1, size=100)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Model, Hyperparameters, ExperimentLogger options')
 
-val_size = 0.2 # -- > replace with valsize Parameter object for experiment logging
-if val_size > 0.0:
-    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-    for train_idx, val_idx in sss.split(X, y):
-        X_train, X_val = X[train_idx], X[val_idx]
-        y_train, y_val = y[train_idx], y[val_idx]
-else:
-    X_train = X
-    y_train = y
-
-# ====================
-# Within Subject 5-CV
-# ====================
-n_iter = 100    # --> Update to Parameter object
-eval_inter = 5  # --> Update to Parameter object
-
-folds_acc = []
-folds_kappa = []
-
-k = 5
-skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=42)
-for fold, (train_idx, test_idx) in enumerate(skf.split(X_train, y_train)):
-    model = DBConformer()       # --> Update ARgs to Parameter object
-    optimizer = torch.optim.Adam(model.parameters(), lr=3E-4, betas=(0.9, 0.99), weight_decay=1E-5)     # --> Update Args to Parameter objects
-    loss_fn = nn.CrossEntropyLoss()
-
-    x_test, y_test = X_train[test_idx], y_train[test_idx]
-    x_train, y_train = X_train[train_idx], y_train[train_idx]
-    train_loader = DataLoader(
-        EEGDataset(x_train, y_train),
-        batch_size=32,
-        shuffle=True
+    parser.add_argument('--model_name', type=str, default='db_conformer',
+        choices=[
+            'db_conformer',
+            'mtf_c',
+            'dual_tsst'
+        ]
         )
-    test_loader = DataLoader(
-        EEGDataset(x_test, y_test),
-        batch_size=32,
-        shuffle=True
-        )
+    parser.add_argument('--dataset', type=str, default='dummy_dataset',
+        choices=['dummy_dataset', 'BNCI2014_001']
+    )
+    parser.add_argument('--subject', type=int, default=1)
+    parser.add_argument('--device', type=str, default='cpu',
+        choices = ['cpu', 'cuda']
+    )
+    parser.add_argument('--verbose', action='store_true',
+        help='Print training progress')
 
-    # Training and Evaluate CV-folds for n_iter epochs
-    best_acc_per_fold = 0
-    best_kappa_per_fold = -1
-    for i in tqdm(range(n_iter), desc=f"Training: fold {fold+1}/{k}"):
-        # Train and upadte train folds performance
-        model.train
-        train_loss = 0
-        train_acc = 0
-        for j, (x, y) in enumerate(train_loader):
-            x, y = x.to(device), y.to(device)
-            optimizer.zero_grad()
-            representations, logits = model(x)
-            acc = accuracy_score(logits, y.cou().detach().numpy())
+    return parser.parse_args()
 
-            loss = loss_fn(logits, y)
-            loss.backward()
-            optimizer.step()
+def main():
+    args = parse_args()
+    device = args.device  # --> Update to Parameter object
+    verbose = args.verbose  # --> set to command line argument
 
-            train_loss += loss.item()
-            train_acc += acc.item()
+    # ====================
+    # DATA LOADING & SPLITTING
+    # ====================
+    if args.dataset == 'dummy_dataset':
+        X = np.random.randn(100, 1, 3, 1062)      # --> Replacce with loader class from utils.dataset_loader 
+        y = np.random.randint(0, 2, size=100)
 
-        train_loss /= len(train_loader)
-        train_acc /= len(train_loader)
+        dataset_info = {
+        "n_ch": 3,
+        "n_times": 1062,
+        "n_classes": 2
+    }
+    else:
+        if args.dataset == 'BNCI2014_001':
+            X, y, dataset_info = load_BNCI2014_001(subject=args.subject)
 
-        if i==0 or (i+1)%eval_inter==0 or i==n_iter-1:
-            model.eval()
-            test_accs = []
-            test_losses = []
-            for j, (x, y) in enumerate(test_loader):
+    # --> Replace with laod from yaml file
+    hyperparameters = Namespace(
+        val_size=0.0,
+        n_iter=100,
+        eval_inter=20,
+        folds=3
+    )
+
+    val_size = hyperparameters.val_size # -- > replace with valsize Parameter object for experiment logging
+    if val_size > 0.0:
+        sss = StratifiedShuffleSplit(n_splits=1, test_size=val_size, random_state=42)
+        for train_idx, val_idx in sss.split(X, y):
+            X_train, X_val = X[train_idx], X[val_idx]
+            y_train, y_val = y[train_idx], y[val_idx]
+    else:
+        X_train = X
+        y_train = y
+
+    # ====================
+    # Within Subject 5-CV
+    # ====================
+    n_iter = hyperparameters.n_iter    # --> Update to Parameter object
+    eval_inter = hyperparameters.eval_inter  # --> Update to Parameter object
+
+    folds_acc = []
+    folds_kappa = []
+
+    # --> Replace with laod from yaml file
+    model_args = Namespace(
+        # Data configuration
+        data_name=args.dataset,
+        chn=dataset_info["n_ch"],                    # Number of channels (matches your X shape)
+        time_sample_num=dataset_info["n_times"],     # Number of time points (matches your X shape)
+        class_num=dataset_info["n_classes"],              # Binary classification
+        
+        # Patch configuration
+        patch_size=100,           
+        spa_dim=16,               # Spatial dimension for channel embedding
+        
+        # Model flags
+        gate_flag=False,          # Use gated fusion (paper default: False)
+        posemb_flag=True,         # Use positional embeddings (paper default: True)
+        branch='all',             # Options: 'all', 'temporal', 'spatial' (paper default: 'all')
+        chn_atten_flag=True       # Use channel attention (paper default: True)
+    )
+
+    k = hyperparameters.folds
+    skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=42)
+    for fold, (train_idx, test_idx) in enumerate(skf.split(X_train, y_train)):
+        model = DBConformer(# --> Update ARgs to Parameter object
+            model_args,
+            emb_size=40,      # Embedding dimension (paper default)
+            tem_depth=6,      # Temporal transformer depth (paper default: 5-6)
+            chn_depth=6,      # Spatial transformer depth (paper default: 5-6)
+            chn=3,            # Number of channels (redundant but needed)
+            n_classes=2       # Number of classes (redundant but needed)
+            ) 
+        optimizer = torch.optim.Adam(model.parameters(), lr=3E-4, betas=(0.9, 0.99), weight_decay=1E-5)     # --> Update Args to Parameter objects
+        loss_fn = nn.CrossEntropyLoss()
+
+        _x_test, _y_test = X_train[test_idx], y_train[test_idx]
+        _x_train, _y_train = X_train[train_idx], y_train[train_idx]
+        train_loader = DataLoader(
+            EEGDataset(_x_train, _y_train),
+            batch_size=32,
+            shuffle=True
+            )
+        test_loader = DataLoader(
+            EEGDataset(_x_test, _y_test),
+            batch_size=32,
+            shuffle=True
+            )
+
+        # Training and Evaluate CV-folds for n_iter epochs
+        best_acc_per_fold = 0
+        best_kappa_per_fold = -1
+        for i in tqdm(range(n_iter), desc=f"Training: fold {fold+1}/{k}"):
+            # Train and upadte train folds performance
+            model.train
+            train_loss = 0
+            train_acc = 0
+            for j, (x, y) in enumerate(train_loader):
                 x, y = x.to(device), y.to(device)
-                with torch.no_grad():
-                    # logits, loss, acc = fine_tune_run(encoder, model, clf_head, x, y, LBL_SMOOTH=LBL_SMOOTH)
-                    representations, logits = model(x)
-                    acc = accuracy_score(logits, y.cpu().detach().numpy())
-                    loss = loss_fn(logits, y)
+                y = y.long()
+                optimizer.zero_grad()
+                representations, logits = model(x)
+                acc = accuracy_score(logits, y.cpu().detach().numpy())
 
-                    test_accs.append(acc)
-                    test_losses.append(loss.item())
+                loss = loss_fn(logits, y)
+                loss.backward()
+                optimizer.step()
 
-            fold_acc = torch.mean(test_accs).item()
-            fold_kappa = (fold_acc - 0.5) / (1 - 0.5)
+                train_loss += loss.item()
+                train_acc += acc.item()
 
-            if fold_acc > best_acc_per_fold:
-                best_acc_per_fold = fold_acc
-                best_kappa_per_fold = fold_kappa
-                
-            if verbose:
-                print(f"Acc:{fold_acc}, Kappa:{fold_kappa}")
+            train_loss /= len(train_loader)
+            train_acc /= len(train_loader)
 
-    folds_acc.append(best_acc_per_fold)
-    folds_kappa.append(best_kappa_per_fold)
+            if i==0 or (i+1)%eval_inter==0 or i==n_iter-1:
+                model.eval()
+                test_accs = []
+                test_losses = []
+                for j, (x, y) in enumerate(test_loader):
+                    x, y = x.to(device), y.to(device)
+                    y = y.long()
+                    with torch.no_grad():
+                        # logits, loss, acc = fine_tune_run(encoder, model, clf_head, x, y, LBL_SMOOTH=LBL_SMOOTH)
+                        representations, logits = model(x)
+                        acc = accuracy_score(logits, y.cpu().detach().numpy())
+                        loss = loss_fn(logits, y)
+
+                        test_accs.append(acc)
+                        test_losses.append(loss.item())
+
+                fold_acc = np.mean(test_accs).item()
+                fold_kappa = (fold_acc - 0.5) / (1 - 0.5)
+
+                if fold_acc > best_acc_per_fold:
+                    best_acc_per_fold = fold_acc
+                    best_kappa_per_fold = fold_kappa
+                    
+                if verbose:
+                    print(f"Acc:{fold_acc}, Kappa:{fold_kappa}")
+
+        folds_acc.append(best_acc_per_fold)
+        folds_kappa.append(best_kappa_per_fold)
+
+if __name__ == '__main__':
+    main()
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,8 +1,13 @@
+import numpy as np
+import torch
 from torch.utils.data import Dataset
+
+from moabb.datasets import BNCI2014_001
+from moabb.paradigms import MotorImagery
 
 class EEGDataset(Dataset):
     def __init__(self, data, labels):
-        self.signals = data
+        self.signals = torch.tensor(data, dtype=torch.float32)
         self.labels = labels
     
     def __len__(self):
@@ -11,3 +16,32 @@ class EEGDataset(Dataset):
     def __getitem__(self, idx):
         # Return a single sample
         return self.signals[idx], self.labels[idx]
+
+def load_BNCI2014_001(subject, preprocessing_pipeline=None):
+    dataset = BNCI2014_001()
+    paradigm = MotorImagery(channels=['Fz', 'FCz', 'C3', 'C4', 'Cz', 'Pz', 'P1', 'P2'], resample=250)
+    if isinstance(subject, int):
+        s_x, s_y, metadata = paradigm.get_data(dataset, subjects=[subject])
+    else:
+        raise ValueError('subject argument should be an interger within valid range on MOABB site')
+
+    left_imagery_idx = np.where(s_y=='left_hand')[0]
+    right_imagery_idx = np.where(s_y=='right_hand')[0]
+    left_imagery = s_x[left_imagery_idx]
+    right_imagery = s_x[right_imagery_idx]
+    s_x, s_y = 0, 0
+    X = np.vstack((left_imagery, right_imagery))
+    y = np.hstack((np.zeros(left_imagery.shape[0]), np.ones(right_imagery.shape[0])))    
+
+    if preprocessing_pipeline is not None:
+        X = preprocessing_pipeline(X)
+
+    n_trials, n_ch, n_times = X.shape
+    info = {
+        "n_trials": n_trials,
+        "n_ch": n_ch,
+        "n_times": n_times,
+        "n_classes": len(np.unique(y))
+    }
+    
+    return X, y, info
