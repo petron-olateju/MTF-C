c5248f9bf4c5348e2394ba4a6c5720bf39893b66
emmanuel-olateju <olatejuemmanuel@gmail.com>
2026-01-18 20:21:54 +0200

    remove past elbowMI files

--- dataset/convert_gdf_to_mat.m
+++
@@ -1,9 +0,0 @@
-function convert_gdf_to_mat(s, h, output_filename)
-    data.signal = s;
-    data.marker = h.EVENT.TYP;
-    data.marker_ts = h.EVENT.POS;
-    data.fs = h.SampleRate;
-    data.ch_type = h.Label;
-    save(output_filename, 'data', '-v7');
-    disp(['MAT file created successfully: ', output_filename]);
-end
