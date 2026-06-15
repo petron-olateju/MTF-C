8f40247e8297610f344ba0a4732a32dc16565e2f
petron-olateju <oluwasegun@aims.ac.za>
2026-06-15 08:36:28 +0000

    update run_experiment.sh

--- sh/run_experiment.sh
+++ sh/run_experiment.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 source .venv/bin/activate
-
-uv run python run_experiment.py \
-    --script "cross_validation" \
-    --model_name "mtf_c" \
-    --dataset "mi" \
-    --device "cuda" \
-    --experiment_version "MTFC+FilterBanks_MI_CV" \
-    --experiment_description "MTFC MOTOR_IMAGERY benchmark on MI datasets in 4 fold CV mode" \
-    --messages "use 5 repeats (seeds) of five-fold CV per subject"
+uv run python main.py --validation_strategy cv --dataset_name BNCI2014_001 --model_name db_conformer
