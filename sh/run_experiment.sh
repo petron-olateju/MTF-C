80fcf98931e2d477ec8853bc99262aaad9c8426c
petron-olateju <oluwasegun@aims.ac.za>
2026-06-21 22:13:28 +0000

    per branch attention pooling not beneficial

--- sh/run_experiment.sh
+++ sh/run_experiment.sh
@@ -2,4 +2,4 @@
 set -x
 
 source .venv/bin/activate
-uv run python main.py --validation_strategy loso --dataset_name BNCI2014_004 --model_name db_conformer
+uv run python main.py --validation_strategy loso --dataset_name Cattan2019_PHMD --model_name mtf_c
