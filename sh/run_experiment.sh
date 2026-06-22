4c284bdfb21edf9091ce8a8bce327f5ec9d462e4
petron-olateju <oluwasegun@aims.ac.za>
2026-06-22 15:05:24 +0000

    results on cattan with btach_size=64 and reconstruction lamda for MTFC=1.0

--- sh/run_experiment.sh
+++ sh/run_experiment.sh
@@ -2,4 +2,4 @@
 set -x
 
 source .venv/bin/activate
-uv run python main.py --validation_strategy loso --dataset_name Cattan2019_PHMD --model_name mtf_c --experiment_details "Increase batch size to 64  include 0.1*spectrum_reconstruction_loss in mtfc loss"
+uv run python main.py --validation_strategy loso --dataset_name Cattan2019_PHMD --model_name mtf_c --experiment_details "Increase batch size to 64"
