4f9d13277b354988455dcf023212f8c910c56d2e
petron-olateju <oluwasegun@aims.ac.za>
2026-06-23 00:20:15 +0000

    re-run with limiting band_powers of spectrum to 1-30Hz

--- sh/run_experiment.sh
+++ sh/run_experiment.sh
@@ -2,4 +2,4 @@
 set -x
 
 source .venv/bin/activate
-uv run python main.py --validation_strategy loso --dataset_name Cattan2019_PHMD --model_name mtf_c --experiment_details "Increase batch size to 64  frequency spectrum reconstruction with channel and temporal attention pooling"
+uv run python main.py --validation_strategy loso --dataset_name Cattan2019_PHMD --model_name mtf_c --experiment_details "Increase batch size to 64  frequency spectrum reconstruction (lambda=0.1) with channel attention pooling  fix:restrict compute_bandpowers frequency range to 1-30Hz"
