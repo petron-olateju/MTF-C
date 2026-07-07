6c2f254798632d57b66abaa33c289d6334aa3706
petron-olateju <oluwasegun@aims.ac.za>
2026-07-07 15:23:17 +0200

    add static_config_datasets sweep python script

---
+++ python_experiment_scripts/static_config_datasets_sweep.py
@@ -0,0 +1,28 @@
+import yaml
+import subprocess
+from tqdm import tqdm
+
+DATASETS = [
+    "Cattan2019_PHMD", "Rodrigues2017", 
+    "Nakanishi2015", "Kalunga2016", 
+    "BNCI2014_001", "BNCI2014_002", "BNCI2014_004"
+]
+VALIDATION = "loso"
+OUTPUT_DIR = "experiments/sst_decoding+NRMSE_Loss"
+
+for dataset in tqdm(DATASETS, total=len(DATASETS)):
+
+    EXPERIMENT_DETAILS = f"Transformer depth=2 across all branches  One LOSO repeat  channel_attn_pooling alone  sst_decoder=gated_sst_projection+addition"
+    
+    subprocess.run(
+            [
+                "python",
+                "main.py",
+                "--validation_strategy", VALIDATION,
+                "--dataset_name", dataset,
+                "--model_name", "mtf_r_c",
+                "--experiment_details", EXPERIMENT_DETAILS,
+            "--output_dir", OUTPUT_DIR
+            ],
+            check=True,
+        )
