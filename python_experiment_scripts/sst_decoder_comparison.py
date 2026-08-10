6d1ad9faa43e9247ace50a8b6bf2354780aafc74
petron-olateju <oluwasegun@aims.ac.za>
2026-08-10 07:19:24 +0100

    FIX: sst_decoder_comparison.py; re-run DBConformer alone

--- python_experiment_scripts/sst_decoder_comparison.py
+++ python_experiment_scripts/sst_decoder_comparison.py
@@ -10,51 +10,33 @@ DATASETS = [
     "BNCI2014_001", "BNCI2014_002", "BNCI2014_004"
 ]
 VALIDATION = "loso"
-OUTPUT_DIR = "experiments/sst_decoding+NRMSE_Loss"
+OUTPUT_DIR = "experiments/sst_decoding+NRMSE_Loss+Loss_checkpointing"
+
+DBCONFORMER_SSTs = ['st_addition', 'st_projection+addition', 'st_projection_addition']
+MTFC_SSTs = ['sst_addition', 'sst_projection+addition', 'sst_multi_projection+addition', 'sst_multi_projection+branch_addition', 'sst_cross_attention']
 
 for dataset in tqdm(DATASETS, total=len(DATASETS)):
-
-    # for sst_decoder in ['st_addition', 'st_projection+addition', 'st_projection_addition']:
-    #     MODEL_PARAMS['db_conformer']['sst_decoder'] = sst_decoder
-    #     MODEL_PARAMS['mtf_c']['sst_decoder'] = sst_decoder
-    #     with open('configs/model_params.yaml', 'w') as f:
-    #         yaml.dump(MODEL_PARAMS, f)
-
-    #     EXPERIMENT_DETAILS = f"Transformer depth=2 across all branches  One LOSO repeat  channel_attn_pooling alone  sst_decoder={sst_decoder}"
-
-    #     subprocess.run(
-    #             [
-    #                 "python",
-    #                 "main.py",
-    #                 "--validation_strategy", VALIDATION,
-    #                 "--dataset_name", dataset,
-    #                 "--model_name", "db_r_conformer",
-    #                 "--experiment_details", EXPERIMENT_DETAILS,
-    #             "--output_dir", OUTPUT_DIR
-    #             ],
-    #             check=True,
-    #         )
-        
-    #     subprocess.run(
-    #             [
-    #                 "python",
-    #                 "main.py",
-    #                 "--validation_strategy", VALIDATION,
-    #                 "--dataset_name", dataset,
-    #                 "--model_name", "mtf_r_c",
-    #                 "--experiment_details", EXPERIMENT_DETAILS,
-    #             "--output_dir", OUTPUT_DIR
-    #             ],
-    #             check=True,
-    #         )
-        
-    # for sst_decoder in ['sst_addition', 'sst_projection+addition', 'gated_sst_projection+addition']:
-    for sst_decoder in ['sst_multi_projection+branch_addition']:
+    # RUN FOR DBCONFORMER
+    for sst_decoder in DBCONFORMER_SSTs:
+        MODEL_PARAMS['db_conformer']['sst_decoder'] = sst_decoder
         MODEL_PARAMS['mtf_c']['sst_decoder'] = sst_decoder
         with open('configs/model_params.yaml', 'w') as f:
             yaml.dump(MODEL_PARAMS, f)
 
         EXPERIMENT_DETAILS = f"Transformer depth=2 across all branches  One LOSO repeat  channel_attn_pooling alone  sst_decoder={sst_decoder}"
+
+        subprocess.run(
+                [
+                    "python",
+                    "main.py",
+                    "--validation_strategy", VALIDATION,
+                    "--dataset_name", dataset,
+                    "--model_name", "db_r_conformer",
+                    "--experiment_details", EXPERIMENT_DETAILS,
+                "--output_dir", OUTPUT_DIR
+                ],
+                check=True,
+            )
         
         subprocess.run(
                 [
@@ -68,3 +50,27 @@ for dataset in tqdm(DATASETS, total=len(DATASETS)):
                 ],
                 check=True,
             )
+
+
+
+# for dataset in tqdm(DATASETS, total=len(DATASETS)):
+#     # RUN FOR MTFC
+#     for sst_decoder in MTFC_SSTs:
+#         MODEL_PARAMS['mtf_c']['sst_decoder'] = sst_decoder
+#         with open('configs/model_params.yaml', 'w') as f:
+#             yaml.dump(MODEL_PARAMS, f)
+
+#         EXPERIMENT_DETAILS = f"Transformer depth=2 across all branches  One LOSO repeat  channel_attn_pooling alone  sst_decoder={sst_decoder}"
+        
+#         subprocess.run(
+#                 [
+#                     "python",
+#                     "main.py",
+#                     "--validation_strategy", VALIDATION,
+#                     "--dataset_name", dataset,
+#                     "--model_name", "mtf_r_c",
+#                     "--experiment_details", EXPERIMENT_DETAILS,
+#                 "--output_dir", OUTPUT_DIR
+#                 ],
+#                 check=True,
+#             )
